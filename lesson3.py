68d17eeb774f59684c7eb20e261c275be1ba188d
GagYedigaryan <edigaryan-gag@mail.ru>
2020-10-15 18:48:59 +0400

    first commit

---
+++ lesson3.py
@@ -0,0 +1,107 @@
+# name = 'Aram'
+# print(name)
+# x = 5
+# y = 7
+# result = x + y
+# print(result)
+# lastname = 'Gevorgyan'
+# print(lastname)
+# mph = 16
+# print(mph)
+# mark = 'bmw'
+# print(mark)
+# name2 ='jesica'
+# print(name)
+# project = 'ferrari'
+# print(project)
+# team='websoft'
+# print(team)
+# x=6
+# y=1
+# z=0.5
+# result=x+y+z
+# print(result)
+# file='new'
+# print(file)
+# brand='samsung'
+# print(brand)
+# phone = 'acer'
+# print(phone)
+# eror='404'
+# print(eror)
+# brouser='google'
+# print(brouser)
+
+
+# x = input('greq tiv ')
+# print(x)
+# y=input('data born ')
+# print(y)
+# x5=input('bmw')
+# print(bmw)
+# x=13
+# y=5
+# result=x % y
+# # print(result)
+# x=-15
+# y=5
+# result=x%y
+# print(result)
+# x=10
+# y=3
+# result =x//y
+# print(result)
+# x=15
+# y=5
+# result=x//y
+# print(result)
+# x=4
+# y=3
+# results = x**y
+# print(results)
+# x=4
+# result=x**0.5
+# print(result)
+# balance=0
+# paycheck=150
+# newbalance=balance+paycheck
+# print(newbalance)
+# balance=100
+# balance+=150
+# print(balance)
+# balance=0
+# print('\n\t\t     barew dzez')
+# yntrutyun=input('\n\t\t  avelacnel gumar? (y/n)')
+# print(yntrutyun)
+# gumarichapy=int(input('\n\t gumari chapy'))
+# balance+=gumarichapy
+# print('der hashivne',balance, '$')
+# x=5
+# y=7
+# print(x>y)
+# print(x==y)
+# balance=1000
+# hanvoxgumary=10000
+# print(hanvoxgumary<=balance)
+# classeone=int(input('how much people have your class'))
+# print(classeone)
+# maxpeople=16
+# result=classeone>maxpeople
+# print(result)
+# oldoreven=classeone%2
+# print(oldoreven==0)
+# classeone-=1
+# firstclasse=classeone/2
+# secondclasss=firstclasse+1
+# firstclasse=print(secondclasss)
+timedischarging=int(input('time when discharging?'))
+print(timedischarging)
+morning=int(input('when i whakeup'))
+print(morning)
+
+day1=int(input('when i whakeup'))
+day2=int(input('when i whakeup'))
+day3=int(input('when i whakeup'))
+result=(day1+day2+day3)/3
+print(result == morning,'100%')
+
