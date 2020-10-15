33ef8cfc0ba179d3ea2cbc12926ca21e85456135
GagYedigaryan <edigaryan-gag@mail.ru>
2020-10-15 19:18:19 +0400

    add python

---
+++ lesson7.py
@@ -0,0 +1,49 @@
+# color = input('rithe color phone ')=='red'
+# model = input('model  phone')=='iphone 7'
+# result=color and model
+# print('buy phone',result)
+
+
+# queshon1=input('\n\t\t  qani kilogram e mi funty? ')=='0.4'
+# queshon2=input('\n\t\t  inj guyn uni tiezerakan xorochy')=='sev'
+# queshon3=input('\n\t\t  qani molorak ka  aregaknayin hamakargum')=='9'
+# queshon4=input('\n\t\t  hayastani mayraqaxaqy')=="yerevan"
+# queshon5=input('\n\t\t  qani metr e 1 killometery')=='1000'
+# result=queshon1 and queshon2 and queshon3 and queshon4 and queshon5
+# print(result)
+
+# num=input('3 >   ')
+# rigth_answer='4 3 2 1'
+# result=num in rigth_answer
+# print(result)
+# poqrtariq = 17
+# mectariq = 25
+# imacutyun ='pyton'
+# krtutyun = 'barcraguyn'
+
+# tariq = int(input('dzer tariqy'))
+# payman3 = input('barcraguyn krtuytyun ')==krtutyun
+# payman4 = input('inj cragravorman lezu giteq ')==imacutyun
+
+# result = poqrtariq <  tariq  and  tariq < mectariq
+# result2 = payman3 and payman4
+
+# print('yntunvac eq', result and result2)
+
+
+
+# car = 'bmw  audi   mersedes porche  toyota  nissan  shkoda vag bentley ROLS ROYCE'
+# date = '1996  1997  1998  2003  2006  2008  2010  2014   2011  2018 '
+# color = 'black  white  red  gray  grean  blue  titan_gray  chery_red  wed_black'
+# buycar=input('buy car')
+# car_date=input('car date')
+# car_color=input('car color')
+# result = buycar in car and car_date in date  and car_color in color 
+# print(result)
+
+
+# result = (5+6) ** 3 = ?
+# result2=
+
+
+
